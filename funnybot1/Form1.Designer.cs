3c2f68dc08a8a186f63f69b57faa17528fc55320
szdanika <danikaszab@gmail.com>
2022-03-10 11:46:47 +0100

    Add project files.

---
+++ funnybot1/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace funnybot1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 107);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(145, 300);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ funnybot1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+
+namespace funnybot1
+{
+    public partial class Form1 : Form
+    {
+        bool isRunning = true;
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Elkuldtem egyszer");
+            SendKeys.Send("hehe");
+            SendKeys.Send("{ENTER}");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+            Thread th = new Thread(Keyboardd);
+            th.Start();
+        }
+        void Keyboardd()
+        {
+            while(isRunning)
+            {
+
+                Thread.Sleep(15);
+                //SendKeys.Send("Kecske");
+                //MessageBox.Show("Bent vagyok");
+                if (Keys.A == (Keys.A))
+                {
+                    //MessageBox.Show("elindult");
+                    //timerstart();
+                }
+                //if (Control.ModifierKeys == Keys.F1)
+                    //timerStop();
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+                if (IsInputKey(Keys.Escape))
+                {
+                    textBox1.Text = "Érzékeltem a gombot!";
+                    timer1.Start();
+                }
+                else
+                    timer1.Stop();
+            */
+        }
+        public void timerstart()
+        {
+            timer1.Start();
+        }
+        public void timerStop()
+        {
+            timer1.Stop();
+        }
+    }
+}
